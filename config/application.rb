e0fd06934a780cb699810a8eb406d5f06125947a
Michal Poczwardowski <michal.poczwardowski@netguru.pl>
2018-02-23 23:40:16 +0100

    Use dotenv for configuration do not keep anything in the repo
    Also for development environmant config is ENV driven

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,8 @@ Bundler.require(*Rails.groups)
 
 module TravelBlog
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_mailer.default_url_options = { host: Rails.application.secrets.mailer_default_url }
   end
 end
