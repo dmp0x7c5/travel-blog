a5915ad31f55a003b99c445a66e4d9d53fd39651
Michal Poczwardowski <michal.poczwardowski@netguru.pl>
2018-02-24 19:29:32 +0100

    Clear rubocop issues in app/admin namespace

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
@@ -24,5 +26,4 @@ ActiveAdmin.register User do
     end
     f.actions
   end
-
 end
