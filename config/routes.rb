c0b8ec9c9c290a84306fb4ead258c7b45e15985f
Michal Poczwardowski <michal.poczwardowski@netguru.pl>
2018-02-25 23:35:40 +0100

    Mount grape and swagger to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount API::Root, at: "/"
+
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  mount GrapeSwaggerRails::Engine => "/swagger"
 end
