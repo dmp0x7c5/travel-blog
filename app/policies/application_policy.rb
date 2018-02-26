abfe55da6b7ecba1581064aab54cd959caaae345
Michal Poczwardowski <michal.poczwardowski@netguru.pl>
2018-02-26 23:59:09 +0100

    Cleanup ApplicationPolicy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -8,34 +8,6 @@ class ApplicationPolicy
     @record = record
   end
 
-  def index?
-    false
-  end
-
-  def show?
-    scope.where(id: record.id).exists?
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
   def scope
     Pundit.policy_scope!(user, record.class)
   end
